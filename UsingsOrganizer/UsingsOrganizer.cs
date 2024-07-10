de8af04b38f1dd00987bfed21ef151e5e7a1e41c
rakichkin <rakichkin@gmail.com>
2024-07-10 21:45:32 +0300

    Implemented a method to find the most frequently occurring line separator in text

--- UsingsOrganizer/UsingsOrganizer.cs
+++ UsingsOrganizer/UsingsOrganizer.cs
@@ -1,4 +1,10 @@
-﻿namespace UsingsOrganizer;
+﻿#nullable enable
+
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace UsingsOrganizer;
 
 public static class UsingsOrganizer
 {
@@ -6,6 +12,36 @@ public static class UsingsOrganizer
 	{
 		if(options == null) options = new UsingsOrganizerOptions();
 
+		var separator = FindMostFrequentLineSeparator(rawUsingsText);
+		if(separator == null) return rawUsingsText;
+
+		var lines = rawUsingsText.Split([separator], StringSplitOptions.RemoveEmptyEntries).ToList();
+
 		throw new NotImplementedException();
+	}
+
+	private static string? FindMostFrequentLineSeparator(string text)
+	{
+		var separatorCounts = new Dictionary<string, int>()
+		{
+			{ "\r\n", 0 },
+			{ "\n", 0 },
+			{ "\r", 0 }
+		};
+		for(int i = 0; i < text.Length; i++)
+		{
+			foreach(var sep in separatorCounts.Keys)
+			{
+				if(i + sep.Length <= text.Length && text.Substring(i, sep.Length) == sep)
+				{
+					separatorCounts[sep]++;
+					i += sep.Length - 1; // перескакиваем через разделитель
+					break;
+				}
+			}
+		}
+
+		if(separatorCounts.All(c => c.Value == 0)) return null;
+		return separatorCounts.Where(kvp => kvp.Value == separatorCounts.Max(x => x.Value)).First().Key;
 	}
 }
--- UsingsOrganizer/UsingsOrganizerOptions.cs
+++ UsingsOrganizer/UsingsOrganizerOptions.cs
@@ -1,23 +1,5 @@
 ﻿namespace UsingsOrganizer;
 
-public enum LinesDelimiters
-{
-	CR,
-	LF,
-	CRLF
-}
-
 public record UsingsOrganizerOptions
 {
-	public LinesDelimiters LinesDelimiters { get; set; } = LinesDelimiters.CRLF;
-
-	public UsingsOrganizerOptions()
-	{
-
-	}
-
-	public UsingsOrganizerOptions(LinesDelimiters linesDelimiter)
-	{
-		LinesDelimiters = linesDelimiter;
-	}
 }
